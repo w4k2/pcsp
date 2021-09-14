5ec542030e0e8040e5e9ff83ad8063e08f3154e2
Szymon Wojciechowski <szymon.wojciechowski@pwr.edu.pl>
2021-09-14 15:22:39 +0200

    animateateodeocredo

--- _clusters.py
+++ _clusters.py
@@ -17,18 +17,25 @@ import numpy as np
 
 from tqdm import tqdm
 
+plt.rcParams.update({'font.size': 7})
+
 N_CHUNKS = 100
 
 ESTIMATORS = [
-    Birch,
-    KMeans,
-    SpectralClustering,
-    AgglomerativeClustering,
+    Birch(),
+    KMeans(),
+    SpectralClustering(),
+    AgglomerativeClustering(),
 ]
 
-g = 3
+g = 2
 
 scores = np.zeros((N_CHUNKS, len(ESTIMATORS)))
+
+def make_gif():
+    import subprocess
+    cmd = "convert -delay 10 -loop 0 $(ls -1 gif/*.png | sort -V) foo.gif"
+    subprocess.run(cmd, shell=True)
 
 def main():
     X, y = make_moons(n_samples=10000, random_state=100)
@@ -41,14 +48,18 @@ def main():
     stream = FoldingStationaryStreamGenerator(X, y, n_chunks=N_CHUNKS)
 
     for row, (X_test, y_test) in tqdm(enumerate(stream.generate()), total=N_CHUNKS):
-        fig, axs = plt.subplots(1, len(ESTIMATORS) + 1, figsize=((1 + len(ESTIMATORS) * g), g))
-        axs[0].set_title(f"chunk{row}")
-        axs[0].scatter(*X_test.T, c=y_test, alpha=0.5)
-        axs[0].set_xlim(*x_lim)
-        axs[0].set_ylim(*y_lim)
+        fig = plt.figure(figsize=((1 + len(ESTIMATORS) * g), 2 * g))
+        gs = fig.add_gridspec(2, 1 + len(ESTIMATORS))
+
+        ax = fig.add_subplot(gs[0, 0])
+        ax.set_title(f"chunk [{row}]")
+        ax.scatter(*X_test.T, c=y_test, s=2, alpha=0.7)
+        ax.set_xlim(*x_lim)
+        ax.set_ylim(*y_lim)
 
         for col, est in enumerate(ESTIMATORS):
-            estimator = est(n_clusters=n_clusters)
+            estimator = clone(est)
+            estimator.set_params(n_clusters=n_clusters)
             estimator.fit(X_test)
 
             if hasattr(estimator, 'labels_'):
@@ -56,28 +67,35 @@ def main():
             else:
                 y_pred = estimator.predict(X)
 
-            axs[col+1].set_title(est.__name__)
-            axs[col+1].scatter(*X_test.T, c=y_pred)
-            axs[col+1].set_xlim(*x_lim)
-            axs[col+1].set_ylim(*y_lim)
+            ax = fig.add_subplot(gs[0, col + 1])
+
+            ax.set_title(type(est).__name__)
+            ax.scatter(*X_test.T, c=y_pred, s=2, alpha=0.7)
+            ax.set_xlim(*x_lim)
+            ax.set_ylim(*y_lim)
+            plt.setp(ax.get_yticklabels(), visible=False)
 
             scores[row, col] = adjusted_rand_score(y_test, y_pred)
 
+
+        ax = fig.add_subplot(gs[1, :])
+
+        for series in scores.T:
+            ax.plot(series[:row], alpha=0.7)
+
+        ax.legend([
+            type(est).__name__ for est in ESTIMATORS
+        ])
+
+        ax.set_ylim(-1.05, 1.05)
+        ax.set_xlim(-0, N_CHUNKS)
+
         plt.tight_layout()
         plt.savefig(f"gif/{row}.png")
+        plt.savefig(f"foo.png")
         plt.close()
 
-
-    fig, ax = plt.subplots(1, 1)
-    for series in scores.T:
-        ax.plot(series, alpha=0.7)
-
-    ax.legend([
-        est.__name__ for est in ESTIMATORS
-    ])
-
-    ax.set_ylim(-1.05, 1.05)
-    plt.savefig("foo.png")
+    make_gif()
 
 
 if __name__ == '__main__':
--- animataeto.sh
+++
@@ -1,2 +0,0 @@
-convert -delay 10 -loop 0 $(ls -1 gif/*.png | sort -V) stream.gif
-gthumb stream.gif
